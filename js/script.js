8b585765c25e9471f743a7307bef390b99b88fc8
Kunal89204 <kunalkhandelwal108@gmail.com>
2023-12-17 20:51:45 +0530

    default location and night images added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Get HTML elements by their IDs or classes
 let search = document.getElementById("search");
 const ico = document.querySelector(".fa-solid");
 let temp = document.querySelector(".temp-c");
@@ -6,17 +7,24 @@ let country = document.querySelector(".country");
 let time = document.querySelector(".time");
 let condition = document.querySelector(".condition");
 let bg = document.querySelector(".bg");
-let wind_speed = document.querySelector(".wind-speed")
-let wind_dir = document.querySelector(".wind-direction")
-let humidity = document.querySelector(".humidity")
+let wind_speed = document.querySelector(".wind-speed");
+let wind_dir = document.querySelector(".wind-direction");
+let humidity = document.querySelector(".humidity");
+let ico_img = document.querySelector(".ico-img");
+let gust = document.querySelector(".gust");
 
+// Function to fetch weather data
 const fetchdata = () => {
+  // WeatherAPI key and default location (New Delhi if no user input)
   const apiKey = "bc6cdf98ec47468598142603231609";
-  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${search.value}`;
+  const location = search.value || "New Delhi"; // Default to New Delhi if no user input
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
 
+  // Fetch data from the API
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
+      // Update HTML elements with weather data
       temp.innerHTML = data.current.temp_c + "&#176;C";
       loc.textContent = data.location.name;
       time.textContent = data.location.localtime;
@@ -24,31 +32,59 @@ const fetchdata = () => {
       wind_dir.innerHTML = data.current.wind_dir;
       wind_speed.innerHTML = data.current.wind_kph + " km/h";
       humidity.innerHTML = data.current.humidity;
+      ico_img.src = data.current.condition.icon;
+      gust.innerHTML = data.current.gust_kph + "km/h";
 
+      // Get the weather condition text
       let climate = data.current.condition.text;
       condition.textContent = climate;
 
+      // Change background image based on weather condition
+      let is_day = data.current.is_day;
+
       if (climate.toLowerCase().includes("clear")) {
-        bg.style.background = 'url("/img/clear.avif")';
+        bg.style.background = 'url("/img/clear.jpg")';
       } else if (climate.toLowerCase().includes("sunny")) {
         bg.style.background = 'url("/img/sunny.avif")';
-      } else if (climate.toLowerCase().includes("partly cloudy")) {
+      } else if (
+        climate.toLowerCase().includes("partly cloudy") ||
+        (climate.toLowerCase().includes("overcast") && is_day == 1)
+      ) {
         bg.style.background = 'url("/img/cloudy.jpg")';
-      } else if (climate.toLowerCase().includes("snow")) {
+      } else if (
+        climate.toLowerCase().includes("partly cloudy") ||
+        (climate.toLowerCase().includes("overcast") && is_day == 0)
+      ) {
+        bg.style.background = 'url("/img/night/cloudy.avif")';
+      } else if (climate.toLowerCase().includes("snow") && is_day == 1) {
         bg.style.background = 'url("/img/snow.avif")';
-      } else if (climate.toLowerCase().includes("cloud")) {
+      } else if (climate.toLowerCase().includes("snow") && is_day == 0) {
+        bg.style.background = 'url("/img/night/snow.webp")';
+      } else if (climate.toLowerCase().includes("cloud") && is_day == 1) {
         bg.style.background = 'url("/img/cloudy.jpg")';
-      } else if (climate.toLowerCase().includes("mist")) {
+      } else if (climate.toLowerCase().includes("cloud") && is_day == 0) {
+        bg.style.background = 'url("/img/night/cloudy.avif")';
+      } else if (climate.toLowerCase().includes("mist") && is_day == 1) {
         bg.style.background = 'url("/img/mist.jpg")';
+      } else if (climate.toLowerCase().includes("mist") && is_day == 0) {
+        bg.style.background = 'url("/img/night/mist.jpg")';
       } else if (climate.toLowerCase().includes("thunder")) {
         bg.style.background = 'url("/img/thor.jpg")';
-      } else if (climate.toLowerCase().includes("freez")) {
+      } else if (climate.toLowerCase().includes("freez") && is_day == 1) {
         bg.style.background = 'url("/img/freezing-fog.jpg")';
+      } else if (climate.toLowerCase().includes("freez") && is_day == 0) {
+        bg.style.background = 'url("/img/night/snow.webp")';
+      } else if (climate.toLowerCase().includes("rain")) {
+        bg.style.background = 'url("/img/rain.avif")';
       }
-
+      // Set background image properties
       bg.style.backgroundRepeat = "no-repeat";
       bg.style.backgroundSize = "100% 100%";
     });
 };
 
+// Add click event listener to the icon for fetching data
 ico.addEventListener("click", fetchdata);
+
+// Call fetchdata function with default location (New Delhi) on page load
+window.addEventListener("load", fetchdata);
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const fetchdata = () => {
     const apiKey = 'bc6cdf98ec47468598142603231609';
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=new york`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=new york&aqi=yes`;
   
     fetch(apiUrl)
       .then((response) => response.json())
@@ -10,3 +10,5 @@ const fetchdata = () => {
 }
 
 fetchdata();
+
+
